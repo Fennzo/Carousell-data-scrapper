7c3ff39a8d971d50f0c470a838e8306cdae996ae
Alex Jin <yijian36@gmail.com>
2020-08-15 04:10:29 +0800

    Add files via upload
    Fixed a bug where the first checkbox couldn't be checked

--- Selenium.java
+++ Selenium.java
@@ -1,8 +1,7 @@
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -40,98 +39,137 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Selenium {
 
 	public static boolean checkExist(WebDriver driver, String xpath) {
-		 boolean flag = false;
-	        try {
-	        	WebElement element = driver.findElement(By.xpath(xpath));
-	            if (element.isDisplayed() || element.isEnabled())
-	                flag = true;
-	        } catch (NoSuchElementException e) {
-	            flag = false;
-	        } 
-	        return flag;
+		boolean flag = false;
+		try {
+			WebElement element = driver.findElement(By.xpath(xpath));
+			if (element.isDisplayed() || element.isEnabled())
+				flag = true;
+		} catch (NoSuchElementException e) {
+			flag = false;
+		}
+		return flag;
 	}
-	
+
 	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\jinyi\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 
 		try {
-			driver.get(
-					"https://sg.carousell.com/categories/1832/?condition_v2=USED&sc=1202081422020a002a160a0c636f6e646974696f6e5f763222060a04555345442a150a0b636f6c6c656374696f6e7322060a04313833322a160a0c636f6e646974696f6e5f763222060a045553454432090a07616464696461733a0408bbe17242037765624a02656e&search=addidas&sort_by=");
-			driver.manage().window().maximize();
-			Actions act = new Actions(driver); 
+
+			String temp = "nike";
+			// search with keyword + click used
+			driver.get("https://sg.carousell.com/categories/1832/?search=" + temp);
+			Actions act = new Actions(driver);
+
+			System.out.println("Searching for:" + temp);
+			
 			// click show more options
-			act.moveToElement(driver.findElement(
-					By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/form[1]/section[1]/div[2]/div[1]/button[1]"))).click().perform();;
-			int checkBoxSize = driver
-					.findElements(By.xpath("//div[@class='dLZJAZVTAg']//div[2]//div[1]//div[1]//label")).size();
+			act.moveToElement(driver.findElement(By.xpath(
+					"/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/form[1]/section[1]/div[2]/div[1]/button[1]")))
+					.click().perform();
 
-			for ( int i = 1; i < checkBoxSize + 1; i++) {
-				String checkBoxName = driver.findElement(By.xpath(
-						"/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/form[1]/section[1]/div[2]/div[1]/div[1]/label["
-								+ i + "]/div[1]/p[1]"))
-						.getText();
+			List<WebElement> checkBoxes = driver.findElements(By.xpath(
+					"/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/form[1]/section[1]/div[2]/div[1]/div[1]/label"));
+			int checkBoxSize = checkBoxes.size();
+
+			for (int i = 0; i < checkBoxSize; i++) {
 
 				
+				String checkBoxName;
+				WebElement checkBox;
+
+				checkBoxes = driver.findElements(By.xpath(
+						"/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/form[1]/section[1]/div[2]/div[1]/div[1]/label"));
+				checkBox = checkBoxes.get(i);
+				checkBoxName = checkBox.getText();
+
+				WebDriverWait wait = new WebDriverWait(driver, 60);
+				Thread.sleep(3000);
+				wait.until(ExpectedConditions.elementToBeClickable(checkBox));
+
 				// click checkbox
-				act.moveToElement(driver.findElement(By.xpath("//div[@class='dLZJAZVTAg']//div[2]//div[1]//div[1]//label[" + i + "]"))).click().perform();
+				act.moveToElement(checkBox).click().perform();
+
 				int listingsCount = 0;
-				boolean flag = checkExist(driver, "/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/main[1]/div[1]/div[1]/div[2]");
-				
+				boolean flag = checkExist(driver,
+						"/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/main[1]/div[1]/div[1]/div[2]");
+
 				if (!flag) {
-					act.moveToElement(driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/form[1]/section[1]/div[2]/div[1]/div[1]/label[2]"))).click().perform();
-					try {
-						Thread.sleep(500);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					act.moveToElement(driver.findElement(By.xpath("//div[@class='SlZgBjnLiz']//div[3]//div[1]//div[1]//label[2]"))).click().perform();
-				}
-				
-				else {
-				while (driver.findElements(By.xpath(
-						"//button[contains(@class,'_3dxOPpKVs8 _2Hl0nzGgOH _3KEDnFP0dp _3AGrhxH5DS _2UF39lBLOv yYAF4gRW1m')]"))
-						.size() > 0) {
-					
-					WebElement loadMore = driver.findElement(By.xpath(	
-							"//button[contains(@class,'_3dxOPpKVs8 _2Hl0nzGgOH _3KEDnFP0dp _3AGrhxH5DS _2UF39lBLOv yYAF4gRW1m')]"));
-				    
-					act.moveToElement(loadMore).click().perform();
+
+					// click used
+					act.moveToElement(driver.findElement(By.xpath("//div[2]//div[1]//div[1]//label[2]"))).click()
+							.perform();
+
+					System.out.println("Empty page used clicked!");
 				}
 
-				 listingsCount = driver
-						.findElements(By
-								.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/main[1]/div[1]/div[1]/div"))
-						.size();
-				
-                
-				act.moveToElement(driver.findElement(By.xpath("//div[@class='dLZJAZVTAg']//div[2]//div[1]//div[1]//label[" + i + "]"))).click().perform();
-				
-				// click used to refresh page
-				act.moveToElement(driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/form[1]/section[1]/div[3]/div[1]/div[1]/label[2]"))).click().perform();
-				
-				try {
+				else {
+
+
+//					Secondary check in url for correct options selected. Usable when there is a change in algo
+//					String currentUrl = driver.getCurrentUrl();
+//					while ( !currentUrl.contains("USED") || !currentUrl.contains("")) {
+//						
+//					}
+					
+					wait.until(ExpectedConditions
+							.elementToBeClickable(checkBoxes.get(i)));
+					
+					if ( checkBox.isSelected()) {
+						System.out.println("Empty box clicked");
+						checkBox.click();
+					}
+					
 					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+
+					boolean isSelected = driver.findElement(By.xpath(
+							"/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/form[1]/section[1]/div[3]/div[1]/div[1]/label[2]"))
+							.isSelected();
+
+					if (isSelected) {
+
+						System.out.println("Used not selected, selecting it now!");
+						// click used if not selected
+						act.moveToElement(driver.findElement(By.xpath(
+								"/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/form[1]/section[1]/div[3]/div[1]/div[1]/label[2]")))
+								.click().perform();
+					}
+
+					// loadmore
+					while (driver.findElements(By.xpath(
+							"//button[contains(@class,'_3dxOPpKVs8 _2Hl0nzGgOH _3KEDnFP0dp _3AGrhxH5DS _2UF39lBLOv yYAF4gRW1m')]"))
+							.size() > 0) {
+
+						WebElement loadMore = driver.findElement(By.xpath(
+								"//button[contains(@class,'_3dxOPpKVs8 _2Hl0nzGgOH _3KEDnFP0dp _3AGrhxH5DS _2UF39lBLOv yYAF4gRW1m')]"));
+
+						act.moveToElement(loadMore).click().perform();
+					}
+
+					listingsCount = driver
+							.findElements(By.xpath(
+									"/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/main[1]/div[1]/div[1]/div"))
+							.size();
+					wait.until(ExpectedConditions
+							.elementToBeClickable(checkBoxes.get(i)));
+					act.moveToElement(checkBox).click().perform();
+
 				}
-				driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[1]/div[2]/div[1]/form[1]/section[1]/div[3]/div[1]/div[1]/label[2]")).click();
-			}
 				
-				System.out.println("Sizing: " + checkBoxName + " || Number of listings: " + listingsCount);	
+				System.out.println("Sizing: " + checkBoxName + " || Number of listings: " + listingsCount);
 			}
 		}
 
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+		driver.close();
+		driver.quit();
 
-	driver.quit();
-	driver.close();
 	}
 
 }
